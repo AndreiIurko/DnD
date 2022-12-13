8cb7add3c3a1f4c6df23fa042426f55b2ce51dc5
Alexandra Pavlova <pavlovaalexandra29@gmail.com>
2022-12-13 20:12:38 +0300

    some fixes and formatting

--- app/src/main/java/com/andreyyurko/dnd/ui/charactermain/CharacterMainFragment.kt
+++ app/src/main/java/com/andreyyurko/dnd/ui/charactermain/CharacterMainFragment.kt
@@ -2,17 +2,12 @@ package com.andreyyurko.dnd.ui.charactermain
 
 import android.animation.ObjectAnimator
 import android.content.Context
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.PopupWindow
-import androidx.core.animation.doOnEnd
-import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.NavHostFragment
@@ -28,7 +23,7 @@ import dev.chrisbanes.insetter.applyInsetter
 @AndroidEntryPoint
 class CharacterMainFragment : Fragment(R.layout.fragment_character_main) {
     private val viewBinding by viewBinding(FragmentCharacterMainBinding::bind)
-    private lateinit var viewModel : CharacterMainViewModel
+    private lateinit var viewModel: CharacterMainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -60,6 +55,11 @@ class CharacterMainFragment : Fragment(R.layout.fragment_character_main) {
             setupPopupMenu(requireContext())
         }
 
+        viewBinding.settingsButton.setOnClickListener {
+            //findNavController().navigate(R.id.abilitiesFragment)
+            TODO("navigate to changing character stats, abilities, etc.")
+        }
+
         viewBinding.arrowBackImageButton.setOnClickListener {
             findNavController().popBackStack()
         }
@@ -70,9 +70,11 @@ class CharacterMainFragment : Fragment(R.layout.fragment_character_main) {
         viewBinding.classTextView.text = viewModel.shownCharacter.characterInfo.characterClass
         viewBinding.levelTextView.text = viewModel.shownCharacter.characterInfo.level.toString()
         viewBinding.hpEditText.setText(viewModel.shownCharacter.characterInfo.hp.toString())
-        viewBinding.proficiencyTextView.text = "+ ${viewModel.shownCharacter.characterInfo.proficiencyBonus}"
+        viewBinding.proficiencyTextView.text =
+            "+ ${viewModel.shownCharacter.characterInfo.proficiencyBonus}"
         viewBinding.speedTextView.text = "${viewModel.shownCharacter.characterInfo.speed}ft"
-        viewBinding.initiativeTextView.text = "+ ${viewModel.shownCharacter.characterInfo.initiativeBonus}"
+        viewBinding.initiativeTextView.text =
+            "+ ${viewModel.shownCharacter.characterInfo.initiativeBonus}"
         viewBinding.acTextView.text = viewModel.shownCharacter.characterInfo.ac.toString()
     }
 
@@ -84,8 +86,9 @@ class CharacterMainFragment : Fragment(R.layout.fragment_character_main) {
         val high = LinearLayout.LayoutParams.WRAP_CONTENT
         val characterMenu = PopupWindow(parent, wid, high, focus)
 
-        val navController = (childFragmentManager.findFragmentById(R.id.mainFragmentNavigationHost) as NavHostFragment).navController
-        parent.findViewById<LinearLayout>(R.id.actionsLinearLayout).apply{
+        val navController =
+            (childFragmentManager.findFragmentById(R.id.mainFragmentNavigationHost) as NavHostFragment).navController
+        parent.findViewById<LinearLayout>(R.id.actionsLinearLayout).apply {
             onPressAnimation(this)
             this.setOnClickListener {
                 navController.navigate(R.id.action_actionsFragment)
